9a88ff5c85251baa1e22a27384ce48c91a33985b
Jumarinv <jumarinv@unal.edu.co>
2024-02-25 07:18:28 -0500

    Taller3Java1.9
    correccion condicional metodos up, down: canal, volumen

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -34,28 +34,28 @@ public class TV {
 	
 	public void canalUp () {
 		
-		if (estado && canal <= 120) {
+		if (estado && canal < 120) {
 			
 			canal = canal + 1;
 		}
 	}
 	
 	public void canalDown () {
-		if (estado && canal >= 1) {
+		if (estado && canal > 1) {
 			
 			canal = canal - 1;
 		}
 	}
 	
 	public void volumenUp () {
-		if (estado && volumen <= 7) {
+		if (estado && volumen < 7) {
 			
 			volumen = volumen + 1;
 		}
 	}
 	
 	public void volumenDown () {
-		if (estado && volumen >= 0) {
+		if (estado && volumen > 0) {
 			
 			volumen = volumen - 1;
 		}
